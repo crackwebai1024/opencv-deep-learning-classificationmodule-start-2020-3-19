0cce078d611be3d9ddbd48f111dae6fdc74d73df
crackwebai1024 <webtowardapex@outlook.com>
2020-03-22 21:59:44 +0800

    alittle revision

--- preprocessing/simplepreprocessor.py
+++ preprocessing/simplepreprocessor.py
@@ -13,3 +13,4 @@ class SimplePreprocessor:
         #resize the image to a fixed size, ignoring the aspect
         #ratio
         return cv2.resize(image, (self.width, self.height), interpolation=self.inter)
+    #
